f446c40428b5cc1ba069e1191d4eae7df508e79b
baviiii <aseeshan11@gmail.com>
2025-07-02 11:18:53 +0930

    sd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useToast } from "@/hooks/use-toast";
+
 import { Home, Users, Mail, MapPin, Building, Briefcase, StickyNote } from "lucide-react";
 
 const Index = () => {
