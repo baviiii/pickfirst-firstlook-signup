fc7a5232ac73e0201f9f9c04ddbc87cd15f3623a
baviiii <aseeshan11@gmail.com>
2025-07-12 02:33:47 +0930

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/pickfirst-firstlook-signup/' : '/',
+  base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/pickfirst-firstlook-signup/' : '/',
   server: {
     host: "::",
     port: 8080,
