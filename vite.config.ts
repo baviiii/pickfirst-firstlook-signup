975365734eb58c47c7dcda84721ab44350386a16
baviiii <aseeshan11@gmail.com>
2025-07-14 01:22:35 +0930

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/pickfirst-firstlook-signup/' : '/',
   server: {
+    
     host: "::",
     port: 8080,
   },
