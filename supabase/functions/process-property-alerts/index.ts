3bb5c1155a0523e25dfd2f7f4226f31c2b4e1885
baviiii <aseeshan11@gmail.com>
2025-11-03 18:27:26 +1030

    retesting pipleine

--- supabase/functions/process-property-alerts/index.ts
+++ supabase/functions/process-property-alerts/index.ts
@@ -3,7 +3,18 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': [
+    'authorization',
+    'x-client-info',
+    'apikey',
+    'content-type',
+    'accept',
+    'accept-profile',
+    'content-profile',
+    'range',
+    'prefer'
+  ].join(', '),
 }
 
 interface AlertJob {
@@ -142,7 +153,7 @@ async function logFeatureAccessAttempt(
 
 serve(async (req) => {
   if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+    return new Response(null, { headers: corsHeaders })
   }
 
   try {
